db446b44f59194d8b62545cd4af948bd4f9cc3f0
Chris Chares <chris@chares.io>
2016-09-18 14:36:33 -0600

    variable instead of function

--- PersistentUUID/Classes/PersistentUUID.swift
+++ PersistentUUID/Classes/PersistentUUID.swift
@@ -14,30 +14,26 @@ public typealias UUIDResponse = (uuid: String, new: Bool) -> Void
 private let STORAGE_KEY = "app_storage_key"
 
 extension NSUUID {
-    public static func fetchUUID(fn: UUIDResponse) {
-        
+    public static var persistentUUID: String {
         //First we check NSUserDefaults
         if let uuid = NSUserDefaults.standardUserDefaults().objectForKey(STORAGE_KEY) as? String {
-            fn(uuid: uuid, new: false)
-            return
+            return uuid
         }
         
         //Now we check keychain
         let keychain = Keychain(service: NSBundle.mainBundle().bundleIdentifier!)
         if let uuid = keychain[STORAGE_KEY] {
             writeDefaults(uuid)
-            fn(uuid: uuid, new: false)
-            return
+            return uuid
         }
         
-        //:TODO add iCloud check
         // If we're still here we're going to create a new UUID and pass it back
         let uuid: String = NSUUID().UUIDString
         writeDefaults(uuid)
         keychain[STORAGE_KEY] = uuid
-        fn(uuid: uuid, new: true)
+        return uuid
     }
-    
+
     private static func writeDefaults(uuid: String) {
         NSUserDefaults.standardUserDefaults().setObject(uuid, forKey: STORAGE_KEY)
         NSUserDefaults.standardUserDefaults().synchronize()
